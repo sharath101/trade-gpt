cc8c03be5420d0d01c22b605c66a2509ae708193
amans-ingh <amansingh160899@gmail.com>
2024-04-28 17:27:21 +0530

    order object

--- utils/db_models.py
+++ utils/db_models.py
@@ -7,6 +7,7 @@ class APIKey(db.Model):
     secret = db.Column(db.String(100), nullable=False)
     expiry = db.Column(db.DateTime, nullable=True)
     platform = db.Column(db.String(100), nullable=False)
+    trading = db.Column(db.Boolean, nullable=False, default=True)
 
     def __repr__(self):
         return f"APIKey(key={self.key}, secret={self.secret})"
@@ -28,3 +29,27 @@ class MarketHolidays(db.Model):
 
     def __repr__(self):
         return f"MarketHolidays(date={self.date}, exchange={self.exchange})"
+
+
+class OrderBook(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # Primary key in database
+    correlation_id = db.Column(db.String(100), nullable=False)  # Correlation ID
+    client_id = db.Column(db.String(100), nullable=False)  # Client ID from the client
+    order_id = db.Column(db.String(100), nullable=False)  # Order ID from the broker
+    symbol = db.Column(db.String(100), nullable=False)  # Symbol of the stock
+    exchange = db.Column(db.String(100), nullable=False)  # Exchange of the stock
+    quantity = db.Column(db.Integer, nullable=False)  # Quantity of the stock placed
+    price = db.Column(db.Float, nullable=False)  # Price of the order excuted
+    transaction_type = db.Column(db.String(100), nullable=False)  # Buy or Sell
+    order_type = db.Column(db.String(100), nullable=False)  # Market or Limit
+    product_type = db.Column(db.String(100), nullable=False)  # Intraday or Delivery
+    status = db.Column(db.String(100), nullable=False)  # Order status
+    order_created = db.Column(db.DateTime, nullable=True)  # Order created timestamp
+    order_updated = db.Column(db.DateTime, nullable=True)  # Order updated timestamp
+    exchange_timestamp = db.Column(db.DateTime, nullable=True)  # Exchange timestamp
+    bo_profit_val = db.Column(db.Float, nullable=True)  # Bracket order profit value
+    bo_stoploss_val = db.Column(db.Float, nullable=True)  # Bracket order stoploss value
+    order_opened = db.Column(db.Boolean, nullable=True)  # Order opened or not
+
+    def __repr__(self):
+        return f"Order(order_id={self.order_id}, symbol={self.symbol})"
