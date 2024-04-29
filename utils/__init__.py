acf62db2e36fccdaa59e398bdad276bb96a7fa42
Sharath Nair <sharathmappu99@gmail.com>
2024-04-29 17:53:12 +0530

    Update utils folder

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,13 @@
-from .redis_manager import RedisManager
-from flask_sqlalchemy import SQLAlchemy
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.executors.pool import ProcessPoolExecutor, ThreadPoolExecutor
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
-from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-import importlib
+
 from api import app
 
+from .common import adjust_perc
+from .redis_manager import RedisManager
 
 jobstores = {"default": SQLAlchemyJobStore(url=app.config["SQLALCHEMY_DATABASE_URI"])}
 executors = {"default": ThreadPoolExecutor(20), "processpool": ProcessPoolExecutor(5)}
@@ -16,20 +17,3 @@ scheduler = BackgroundScheduler(
 )
 
 redis_instance = RedisManager()
-db = SQLAlchemy(app)
-
-engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
-connection = engine.connect()
-table_models = importlib.import_module("utils.db_models")
-
-if not engine.dialect.has_table(connection, "APIKey"):
-    ORMTable = getattr(table_models, "APIKey")
-    ORMTable.__table__.create(bind=engine, checkfirst=True)
-
-if not engine.dialect.has_table(connection, "Symbol"):
-    ORMTable = getattr(table_models, "Symbol")
-    ORMTable.__table__.create(bind=engine, checkfirst=True)
-
-if not engine.dialect.has_table(connection, "MarketHolidays"):
-    ORMTable = getattr(table_models, "MarketHolidays")
-    ORMTable.__table__.create(bind=engine, checkfirst=True)
---
+++ utils/common.py
@@ -0,0 +1,2 @@
+def adjust_perc(value: float, percent: float) -> float:
+    return value * (1 + percent / 100)
